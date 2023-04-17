4f6046e53d255aa4b04e16f2ee6f4de8fcd16626
blacpeaz <chidiigweonu@gmail.com>
2023-04-17 21:09:33 +0300

    created function to calculate age

--- index.js
+++ index.js
@@ -1,7 +1,31 @@
 const calBtnEl = document.querySelector("#cal-btn");
+const birthdayEl = document.querySelector("#birthday");
+const resultEl = document.querySelector("#result");
 
 function calculateAge() {
-  console.log("clicked");
+  const birthdayValue = birthdayEl.value;
+  if (birthdayValue === "") {
+    alert("Please enter your birthday");
+  } else {
+    let age = getAge(birthdayValue);
+    resultEl.innerHTML = `Your age is ${age} ${age > 1 ? "years" : "year"} old`;
+  }
+}
+
+function getAge(birthdayValue) {
+  const currentDate = new Date();
+  const birthdayDate = new Date(birthdayValue);
+  let age = currentDate.getFullYear() - birthdayDate.getFullYear();
+  const month = currentDate.getMonth() - birthdayDate.getMonth();
+
+  if (
+    month < 0 ||
+    (month === 0 && currentDate.getDate() < birthdayDate.getDate())
+  ) {
+    age--;
+  }
+
+  return age;
 }
 
 calBtnEl.addEventListener("click", calculateAge);
